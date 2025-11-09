c1588367330fe9059340da47a71fadff943dbbe4
Piernik <piernik@piernik.rocks>
2025-11-09 23:26:40 +0100

    Added pause menu

--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var score_label: Label = %Points;
+@onready var pause_menu: MarginContainer = %PauseMenu;
 
 func _ready() -> void:
 	score_label.text = "0";
@@ -10,3 +11,14 @@ func _on_grid_add_points(points: int) -> void:
 	var current_score = int(score_label.text);
 	current_score += points;
 	score_label.text = str(current_score);
+
+func _on_resume_pressed() -> void:
+	pause_menu.visible = false;
+
+
+func _on_quit_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/start_screen.tscn");
+
+
+func _on_pause_button_pressed() -> void:
+	pause_menu.visible = true;
